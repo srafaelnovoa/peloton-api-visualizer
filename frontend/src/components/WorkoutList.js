65a58c21e6b08fb59cca306b8e150cc1fd183e66
srafaelnovoa <rafael.novoa@gmail.com>
2025-03-08 11:12:34 -0500

    correction for handling LaneBreak rides

--- frontend/src/components/WorkoutList.js
+++ frontend/src/components/WorkoutList.js
@@ -1,20 +1,28 @@
 import Button from "react-bootstrap/Button";
 
 export function WorkoutList({ workouts, onSelectWorkout }) {
+  console.log("Workout Data:", workouts);
+  if (workouts.length === 0) return null;
   return (
     <div>
       <h3 className="mt-4 text-center">Cycling</h3>
       <ul>
-        {workouts.map((workout) => (
-          <li key={workout.id}>
-            <Button
-              onClick={() => onSelectWorkout(workout.id)}
-              className="mb-1 text-blue-500 underline"
-            >
-              {workout.peloton.ride.title || "Workout"}
-            </Button>
-          </li>
-        ))}
+        {workouts.map((workout) => {
+          const title =
+            workout.name === "Lanebreak Ride"
+              ? workout?.ride?.title
+              : workout?.peloton?.ride?.title;
+          return (
+            <li key={workout.id}>
+              <Button
+                onClick={() => onSelectWorkout(workout.id)}
+                className="mb-1 text-blue-500 underline"
+              >
+                {title || "Workout"}
+              </Button>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );

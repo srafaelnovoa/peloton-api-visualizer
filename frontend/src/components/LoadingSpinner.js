4df4ebefa9837b2b07544df9bb249abc3fcae868
srafaelnovoa <rafael.novoa@gmail.com>
2025-04-05 09:00:43 -0400

    update display LoadingSpinner

--- frontend/src/components/LoadingSpinner.js
+++ frontend/src/components/LoadingSpinner.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function LoadingSpinner() {
   return (
-    <div className="d-flex justify-content-center align-items-center h-100">
+    <div className="container flex-grow-1 d-flex flex-column align-items-center justify-content-center">
       <div className="spinner-border text-primary" role="status">
         <span className="visually-hidden">Loading...</span>
       </div>

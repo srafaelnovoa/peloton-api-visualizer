b6480b74b781f03ba6a47dbb1cd6a9b49b75070e
srafaelnovoa <rafael.novoa@gmail.com>
2025-04-13 09:38:53 -0400

    add disclaimer css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { PelotonDashboardProvider } from "./contexts/PelotonContext";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import "./App.css";
 
 export default function App() {
   return (
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,13 +2,13 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="bg-dark text-white text-center py-3 mt-4">
-      <p>© SRafaelNovoa — MIT License. See GitHub for source code.</p>
-      <small>
+    <footer className="bg-dark text-white text-center pt-3 mt-4 mb-0 pb-0">
+      <small>© SRafaelNovoa — MIT License. See GitHub for source code.</small>
+      <p className="disclaimer pt-1 mb-0">
         This project is for educational and non-commercial use only. Not
         affiliated with or endorsed by Peloton Interactive, Inc. "Peloton" and
         related trademarks are the property of their respective owners.
-      </small>
+      </p>
     </footer>
   );
 }

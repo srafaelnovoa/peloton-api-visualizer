2101a0b611c86d4bcdf953f8101abcc6dab12805
srafaelnovoa <rafael.novoa@gmail.com>
2025-04-12 21:14:24 -0400

    update footer font size

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,12 +3,12 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="bg-dark text-white text-center py-3 mt-4">
-      © SRafaelNovoa — MIT License. See GitHub for source code.
-      <p>
+      <p>© SRafaelNovoa — MIT License. See GitHub for source code.</p>
+      <small>
         This project is for educational and non-commercial use only. Not
         affiliated with or endorsed by Peloton Interactive, Inc. "Peloton" and
         related trademarks are the property of their respective owners.
-      </p>
+      </small>
     </footer>
   );
 }
